6ff15d580c95aa1392d080e0fb02db668d4990e8
Zachary Kim <zachary.kim@wallawalla.edu>
2025-11-02 23:07:33 -0800

    Adding ESlint config for React + JS linting.

--- cms-app/eslint.config.js
+++ cms-app/eslint.config.js
@@ -1,29 +1,57 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jest from "eslint-plugin-jest";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  // 1) What to ignore
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+    ignores: [
+      "node_modules",
+      "dist",
+      "build",
+      "coverage",
+      ".next",
+      "out"
     ],
+  },
+
+  // 2) Base JS rules
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...jest.environments.globals.globals,
+      },
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
       },
     },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooks,
+      jest,
+    },
+    // Start from recommended rule sets
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
+      ...reactPlugin.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // Common React tweaks
+      "react/react-in-jsx-scope": "off",     // not needed with the new JSX transform
+      "react/prop-types": "off",             // if you don't use PropTypes
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+    settings: {
+      react: { version: "detect" },
     },
   },
-])
+];
