52a46217bbd6597058529fe6e74f51de3c429daa
mchaca <careney64@gmail.com>
2025-11-13 12:03:45 -0800

    Saving my login changes

--- cms-app/src/App.jsx
+++ cms-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AnalyticsPage from './pages/analytics-page';
 import CustomerListPage from './pages/customer-list-page';
 import CustomerSearchPage from './pages/customer-search-page';
@@ -11,7 +11,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/analytics" element={<AnalyticsPage />} />
         <Route path="/customers" element={<CustomerListPage />} />
-        <Route path="/" element={<CustomerSearchPage />} />
+        <Route path="/search" element={<CustomerSearchPage />} /> {/* Add this line */}
+        <Route path="/" element={<LoginPage />} /> {/* This must be LoginPage */}
       </Routes>
     </BrowserRouter>
   );
