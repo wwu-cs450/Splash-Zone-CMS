170f21b2326d8fec779e2c8feb36d35595192c35
Joseph Nguyen <joseph.nguyen@wallawalla.edu>
2025-11-14 10:05:10 -0800

    Removing Unnecessary Whitespacee

--- cms-app/src/main.jsx
+++ cms-app/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
